fff9235b6620c5f7e5ff3c307e2077822d8de7b7
Ashfak Uddin <ashmanpapet@gmail.com>
2022-08-07 12:04:05 -0400

    idk

---
+++ database_scripts/home_page.sql
@@ -0,0 +1,14 @@
+CREATE TABLE home_display(
+	title VARCHAR,
+	links VARCHAR
+);
+
+insert into home_display(title, links) values
+('Bags', '//assets.supremenewyork.com/231243/ca/NpORhjC475k.jpg //assets.supremenewyork.com/231223/ca/3Un7LoAyksg.jpg //assets.supremenewyork.com/231292/ca/0_2Wm0c8qCw.jpg //assets.supremenewyork.com/231260/ca/wB43yc914ug.jpg'),
+('Glasses', '//assets.supremenewyork.com/231455/ca/lSjAGZIHXnE.jpg //assets.supremenewyork.com/231611/ca/WCc1gAVvt3A.jpg //assets.supremenewyork.com/239876/ca/_VqtgWbL1AI.jpg //assets.supremenewyork.com/239691/ca/s3VfkRPtD8k.jpg'),
+('shirts', '//assets.supremenewyork.com/230829/ca/g5nvBWy3aUg.jpg //assets.supremenewyork.com/230829/ca/g5nvBWy3aUg.jpg'),
+('sweatshirts', '//assets.supremenewyork.com/229581/ca/dzowrI8dNfg.jpg //assets.supremenewyork.com/229325/ca/0avJjX9LfoM.jpg //assets.supremenewyork.com/229623/ca/L-ffI3SOcGw.jpg //assets.supremenewyork.com/229325/ca/0avJjX9LfoM.jpg'),
+('Pants', '//assets.supremenewyork.com/230616/ca/hIjGqREUKPk.jpg //assets.supremenewyork.com/230674/ca/_r7lT0cqHKo.jpg //assets.supremenewyork.com/230674/ca/_r7lT0cqHKo.jpg'),
+('Caps', '//assets.supremenewyork.com/240196/ca/Qv4WnxCmkbI.jpg');
+
+select * from home_display;
